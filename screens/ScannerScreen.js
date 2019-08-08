9c6306b6d2636e108e5626233626cc9e79868c27
thevarunraja <varun.raja@outlook.com>
2019-08-08 16:13:31 -0400

    lint cleanup

--- screens/ScannerScreen.js
+++ screens/ScannerScreen.js
@@ -1,8 +1,6 @@
 import React from 'react';
-import { View } from 'react-native';
 
 import { Title } from '../components/Typography';
-import { BackButton } from '../components/Buttons';
 import Layout from '../components/Layout';
 
 export default function ScannerScreen(props) {
