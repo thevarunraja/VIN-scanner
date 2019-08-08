83c8fbc0266fbee6ad2dd9e258908bddf3cea639
thevarunraja <varun.raja@outlook.com>
2019-08-08 14:56:57 -0400

    added firebase

---
+++ firebaseConfig.js
@@ -0,0 +1,14 @@
+import firebase from 'firebase';
+
+const config = {
+  apiKey: 'AIzaSyDjOZ7ui111bO78xo3caT3BnNrBnLc9gS4',
+  authDomain: 'vinscannerapp.firebaseapp.com',
+  databaseURL: 'https://vinscannerapp.firebaseio.com',
+  projectId: 'vinscannerapp',
+  storageBucket: '',
+  messagingSenderId: '1066590212649',
+  appId: '1:1066590212649:web:29a527dd0eda3e60'
+};
+const app = firebase.initializeApp(config);
+
+export default app;
